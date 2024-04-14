a185072964a7094847702ee8e9a0e44ab043cafc
MarianaUserGit <maroleinic@gmail.com>
2024-04-14 19:41:02 +0300

    add homework lesson_3

---
+++ lesson_3/task_2.py
@@ -0,0 +1,36 @@
+# Aceasta este a doua sarcină legată de booleeni în python
+# Creați o variabilă numită `is_student` și setați-o la `True`
+# CODUL TĂU VINE MAI JOS:
+is_student = True
+# CODUL TĂU VINE MAI SUS:
+
+# Acum tipăriți valoarea variabilei `is_student` folosind funcția `print`
+# CODUL TĂU VINE MAI JOS:
+print(f'Value of is_student: {is_student}')
+# CODUL TĂU VINE MAI SUS:
+
+# Acum creați o nouă variabilă numită `is_child` și setați-o la `False` și tipăriți valoarea variabilei `is_child` folosind funcția `print`
+# CODUL TĂU VINE MAI JOS:
+is_child = False
+print(f'Value of is_child: {is_child}')
+# CODUL TĂU VINE MAI SUS:
+
+# Acum utilizați operatorul `and` pentru a verifica dacă atât `is_student`, cât și `is_child` sunt `True` și tipăriți rezultatul folosind funcția `print`
+# CODUL TĂU VINE MAI JOS:
+result_of_and = is_student and is_child
+print(f'Result of is_student and is_child: {result_of_and}')
+# CODUL TĂU VINE MAI SUS:
+
+# Acum utilizați operatorul `or` pentru a verifica dacă cel puțin una dintre `is_student` și `is_child` este `True` și tipăriți rezultatul folosind funcția `print`
+# CODUL TĂU VINE MAI JOS:
+result_of_or = is_student or is_child
+print(f'Result of is_student or is_child: {result_of_or}')
+# CODUL TĂU VINE MAI SUS:
+
+# Acum utilizați operatorul `not` pentru a verifica dacă `is_student` este `False` și tipăriți rezultatul folosind funcția `print`
+# CODUL TĂU VINE MAI JOS:
+result_of_not = not is_student
+print(f'Result of not is_student: {result_of_not}')
+# CODUL TĂU VINE MAI SUS:
+
+# Aceasta a fost tot pentru această sarcină
